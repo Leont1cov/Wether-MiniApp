c0f7d2262c16cdaa6ade0240ddacc7b6e7e923fa
Leont1cov <chuchunovvakov@mail.ru>
2025-10-25 14:20:13 +0300

    refactor: rewritten functionality LicationCard component

--- src/Dashboards/LocationCard.tsx
+++ src/Dashboards/LocationCard.tsx
@@ -1,23 +1,37 @@
 import "../styles/locationCard.scss";
+import {useEffect, useState} from "react";
 
 type LocationCardProps = {
     city: string;
 }
 
 const LocationCard = ({ city }: LocationCardProps) => {
-    const now = new Date();
+    const [time, setTime] = useState("");
 
-    let hours:string | number = now.getHours();
-    let minutes:string | number = now.getMinutes();
+    useEffect(() => {
+        function updateTime(){
+            const now = new Date();
+            let hours:string | number = now.getHours();
+            let minutes:string | number = now.getMinutes();
 
-    hours = hours < 10 ? '0' + hours : hours;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
+            hours = hours < 10 ? '0' + hours : hours;
+            minutes = minutes < 10 ? '0' + minutes : minutes;
+
+            setTime(hours + ':' + minutes);
+        }
+
+        updateTime();
+
+        const interval = setInterval(updateTime,1000);
+        return () => clearInterval(interval);
+    }, []);
+
 
     return (
         <>
             <div className="card locationCard">
                 <h2 className="locationCard__city">{city}</h2>
-                <h1 className="locationCard__time">{`${hours}:${minutes}`}</h1>
+                <h1 className="locationCard__time">{time}</h1>
             </div>
         </>
     )
