64d41c323d4752f7071f137f75a9ca2e2cf72a4f
Leont1cov <chuchunovvakov@mail.ru>
2025-10-25 11:48:44 +0300

    feat: add LocationCard component

---
+++ src/Dashboards/LocationCard.tsx
@@ -0,0 +1,26 @@
+import "../styles/locationCard.scss";
+
+type LocationCardProps = {
+    city: string;
+}
+
+const LocationCard = ({ city }: LocationCardProps) => {
+    const now = new Date();
+
+    let hours:string | number = now.getHours();
+    let minutes:string | number = now.getMinutes();
+
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+
+    return (
+        <>
+            <div className="locationCard">
+                <h2 className="locationCard__city">{city}</h2>
+                <h1 className="locationCard__time">{`${hours}:${minutes}`}</h1>
+            </div>
+        </>
+    )
+}
+
+export default LocationCard;
