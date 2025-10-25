52aa3bcec6a8784f996c75abfd3cfdec775c417b
Leont1cov <chuchunovvakov@mail.ru>
2025-10-25 11:20:41 +0300

    feat: WeatherApi file moved separate component

---
+++ src/services/WeatherApi.ts
@@ -0,0 +1,16 @@
+const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+
+async function WeatherApi (city: string):Promise<unknown> {
+    const url:string = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    if (!city) throw new Error('city not specified');
+    if (!apiKey) throw new Error('apiKey error');
+
+    const response = await fetch(url);
+
+    if(!response.ok) throw new Error('Failed to fetch weather');
+
+    return await response.json();
+}
+
+export default  WeatherApi;
