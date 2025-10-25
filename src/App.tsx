c4c9e0e773efb979fdfda0993b5552856d558210
Leont1cov <chuchunovvakov@mail.ru>
2025-10-25 14:21:59 +0300

    refactor: update API control

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,58 @@ import './App.scss'
 import { useState, useEffect } from "react";
 import weatherApi from "./services/WeatherApi.ts";
 import LocationCard from "./Dashboards/LocationCard.tsx";
+import WeatherDetails from "./Dashboards/WeatherDetails.tsx";
+import WindAndHumidity from "./Dashboards/WindAndHumidity.tsx";
+
+type WeatherData = {
+    name: string;
+    main: {
+        temp: number;
+        feels_like: number;
+        humidity: number;
+    };
+    weather: [
+        { description: string; icon: string }
+    ];
+    wind: {
+        speed: number;
+        deg: number;
+        gust: number;
+    },
+}
 
 function App() {
-    const [weather, setWeather] = useState<unknown | null>(null);
+    const [weather, setWeather] = useState<WeatherData | null>(null);
 
     async function handleWeather(city:string =  "Moscow" ):Promise<void> {
         const data:unknown = await weatherApi(city);
-        setWeather(data);
+        setWeather(data as WeatherData);
     }
 
-    useEffect(():void => {
+    useEffect(() => {
         handleWeather();
+
+        const interval = setInterval(() => {
+            handleWeather();
+        }, 300000);
+
+        return () => clearInterval(interval);
     }, []);
 
   return (
     <>
       <h1>Weather-app</h1>
-        <button>Press button</button>
-        <LocationCard city={weather?.name}/>
+        {weather && <LocationCard city={weather?.name}/>}
+
+        {weather && <WeatherDetails
+            temp={weather.main.temp}
+            feelsLike={weather.main.feels_like}
+            description={weather.weather[0].description}
+            icon={weather.weather[0].icon}/> }
+
+        {weather && <WindAndHumidity deg={weather.wind.deg} gust={weather.wind.gust} speed={weather.wind.speed}/>}
+
+        {weather && weather.sys.sunrise}
     </>
   )
 }
